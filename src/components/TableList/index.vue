43cad168d52f7a3eae12ee5fd1ad8b03e724bd0a
Roberto Alves <roberto@rhfactor.com.br>
2023-02-03 01:43:19 -0300

    Criando componente TableList

---
+++ src/components/TableList/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th v-for="(h,i) in computedHeaders" :key="i">
+          {{ h }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(c,j) in content" :key="j" >
+        <td v-for="(h,i) in computedHeaders" :key="i">{{c[h]}}</td>
+      </tr>
+    </tbody>
+    <tfoot v-if="hasPagination">
+      <tr>
+        <td colspan="100%">
+          <span @click="previewsPage">Anterior</span> | <span @click="nextPage">Próxima</span>
+        </td>
+      </tr>
+    </tfoot>
+  </table>
+
+</template>
+
+<script>
+
+export default {
+  name: 'TableList',
+  props: {
+    headers: {
+      type: Array,
+      default: () => { return [] }
+    },
+    content: {
+      type: Array
+    },
+    totalPages: {
+      type: Number,
+      default: () => { return 1 }
+    }
+  },
+  computed: {
+    computedHeaders () {
+      if (this.headers && this.headers.length > 0) {
+        return this.headers
+      }
+      return Object.keys(this.content[0])
+    },
+    hasPagination () {
+      return this.totalPages > 1
+    }
+  },
+  setup (props, { emit }) {
+    const nextPage = () => {
+      emit('nextPage')
+    }
+    const previewsPage = () => {
+      emit('previewsPage')
+    }
+
+    return {
+      nextPage, previewsPage
+    }
+  }
+}
+</script>
