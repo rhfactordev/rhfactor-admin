0f005ff2f20504aac8211c7307dd9dcc1d4bd7fc
Roberto Fonseca <roberto@rhfactor.com.br>
2023-02-07 01:20:30 -0300

    Ajuste de Layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
-  <header>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> |
-
-      <!-- Adicionar a rotas de autenticação -->
-      <router-link to="/config/sign">Signos</router-link> |
-      <router-link to="/config/planet">Planetas</router-link> |
-      <router-link to="/config/aspect">Aspectos</router-link> |
-      <router-link to="/config/house">Casas</router-link>
-
-    </nav>
+  <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Karana</a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-nav">
+      <div class="nav-item text-nowrap">
+        <a class="nav-link px-3" href="#">Sign out</a>
+      </div>
+    </div>
   </header>
 
-  <router-view/>
+  <div class="container-fluid">
+    <div class="row">
+      <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <div class="position-sticky pt-3 sidebar-sticky">
+          <ul class="nav flex-column">
+            <li v-for="(menu,i) in menus" :key="i" class="nav-item">
+              <router-link :to="menu.path" class="nav-link active" aria-current="page" href="#">
+                <span data-feather="home" class="align-text-bottom"></span>
+                {{ menu.name }}
+              </router-link>
+            </li>
+          </ul>
 
+        </div>
+      </nav>
+
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <router-view />
+      </main>
+    </div>
+  </div>
 </template>
 
+<style>
+.bd-placeholder-img {
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
+
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
+
+.b-example-divider {
+  height: 3rem;
+  background-color: rgba(0, 0, 0, .1);
+  border: solid rgba(0, 0, 0, .15);
+  border-width: 1px 0;
+  box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+}
+
+.b-example-vr {
+  flex-shrink: 0;
+  width: 1.5rem;
+  height: 100vh;
+}
+
+.bi {
+  vertical-align: -.125em;
+  fill: currentColor;
+}
+
+.nav-scroller {
+  position: relative;
+  z-index: 2;
+  height: 2.75rem;
+  overflow-y: hidden;
+}
+
+.nav-scroller .nav {
+  display: flex;
+  flex-wrap: nowrap;
+  padding-bottom: 1rem;
+  margin-top: -1px;
+  overflow-x: auto;
+  text-align: center;
+  white-space: nowrap;
+  -webkit-overflow-scrolling: touch;
+}
+</style>
+
 <script>
+import { menus } from '@/config'
 
 export default {
   components: {
   },
   setup () {
+    return {
+      menus
+    }
   }
 }
 </script>
--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,36 @@
-
 const LOCAL_STORAGE_KEY = {
   production: 'rhfactor_admin_token_prod',
   development: 'rhfactor_admin_token_dev',
   local: 'rhfactor_admin_token_local'
 }
+
+export const menus = [
+  {
+    name: 'Dashboard',
+    path: '/dashboard',
+    menu: true
+  },
+  {
+    name: 'Planetas',
+    path: '/config/planet',
+    menu: true
+  },
+  {
+    name: 'Signos',
+    path: '/config/sign',
+    menu: true
+  },
+  {
+    name: 'Casas',
+    path: '/config/house',
+    menu: true
+  }, {
+    name: 'Aspectos',
+    path: '/config/aspect',
+    menu: true
+  }
+
+]
 
 export const tokenKey = LOCAL_STORAGE_KEY[process.env.NODE_ENV]
 
