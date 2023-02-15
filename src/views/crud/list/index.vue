4f4a7e0de6c9f7079df6b7bd6bac9aecb9abc397
Roberto Fonseca <roberto@rhfactor.com.br>
2023-02-15 10:49:12 -0300

    Fazer reload quando cadastrar

--- src/views/crud/list/index.vue
+++ src/views/crud/list/index.vue
@@ -1,6 +1,6 @@
 <template>
   <modal-factory @close="clear">
-    <crud-form :resource="resource" :schema="schema" :values="editValue" @saved="closeModal" ></crud-form>
+    <crud-form :resource="resource" :schema="schema" :values="editValue" @saved="reload" ></crud-form>
   </modal-factory>
 
   <div class="bg-light">
@@ -23,115 +23,100 @@
   </div>
 </template>
 
-<script>
+<script setup>
 
-import { onMounted, ref, watch } from 'vue'
+import { onMounted, ref, watch, defineProps } from 'vue'
 import TableList from '@/components/TableList/index.vue'
 import service from '@/services'
 import useModal from '@/hooks/useModal'
 import ModalFactory from '@/components/ModalFactory/index.vue'
 import CrudForm from '@/views/crud/form/index.vue'
 
-export default {
-  name: 'CrudListView',
-  components: {
-    CrudForm,
-    ModalFactory,
-    TableList
-  },
-  props: {
-    resource: {
-      type: String,
-      required: true
-    },
-    schema: {
-      type: Object,
-      require: true
-    }
-  },
-  setup (props) {
-    const modal = useModal()
-    const currentPage = ref(0)
-    const editValue = ref({})
+const modal = useModal()
+const currentPage = ref(0)
+const editValue = ref({})
 
-    const getPage = async () => {
-      const { data, errors } = await service.crud.list({
-        resource: props.resource,
-        page: currentPage.value
-      })
-      if (!errors) {
-        page.value = data
-      }
-    }
-
-    const page = ref({
-      content: [],
-      pages: 1
-    })
-
-    const nextPage = async () => {
-      currentPage.value = currentPage.value + 1
-      await getPage()
-    }
-
-    const previewsPage = async () => {
-      currentPage.value = currentPage.value - 1
-      await getPage()
-    }
-
-    const addNewItem = () => {
-      modal.open()
-    }
-
-    const closeModal = () => {
-      modal.close()
-    }
-
-    const edit = async (id) => {
-      const { data, errors } = await service.crud.findOne({
-        resource: props.resource,
-        id
-      })
-      if (data) {
-        editValue.value = data
-        modal.open()
-        return
-      }
-      alert(errors)
-    }
-
-    const clear = () => {
-      console.log('clear')
-      editValue.value = {}
-    }
-
-    watch(
-      () => props.resource,
-      async () => {
-        page.value = {
-          content: [],
-          pages: 1
-        }
-
-        await getPage()
-      }
-    )
-
-    onMounted(() => {
-      getPage()
-    })
-
-    return {
-      page,
-      currentPage,
-      nextPage,
-      previewsPage,
-      addNewItem,
-      edit,
-      editValue,
-      clear,
-      closeModal
-    }
+const getPage = async () => {
+  const { data, errors } = await service.crud.list({
+    resource: props.resource,
+    page: currentPage.value
+  })
+  if (!errors) {
+    page.value = data
   }
 }
+
+const page = ref({
+  content: [],
+  pages: 1
+})
+
+const props = defineProps({
+  resource: {
+    type: String,
+    required: true
+  },
+  schema: {
+    type: Object,
+    require: true
+  }
+})
+
+const nextPage = async () => {
+  currentPage.value = currentPage.value + 1
+  await getPage()
+}
+
+const previewsPage = async () => {
+  currentPage.value = currentPage.value - 1
+  await getPage()
+}
+
+const addNewItem = () => {
+  modal.open()
+}
+
+const closeModal = () => {
+  modal.close()
+}
+
+const reload = () => {
+  closeModal()
+  getPage()
+}
+
+const edit = async (id) => {
+  const { data, errors } = await service.crud.findOne({
+    resource: props.resource,
+    id
+  })
+  if (data) {
+    editValue.value = data
+    modal.open()
+    return
+  }
+  alert(errors)
+}
+
+const clear = () => {
+  console.log('clear')
+  editValue.value = {}
+}
+
+watch(
+  () => props.resource,
+  async () => {
+    page.value = {
+      content: [],
+      pages: 1
+    }
+
+    await getPage()
+  }
+)
+
+onMounted(() => {
+  getPage()
+})
+
 </script>
